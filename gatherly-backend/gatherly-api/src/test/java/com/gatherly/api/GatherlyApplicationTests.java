63c7bfef557d04216929e1f9e0ead5dbfcc84438
SamriekLeeuwin <semriekk@gmail.com>
2025-07-19 20:32:30 +0200

    first commit

---
+++ gatherly-backend/gatherly-api/src/test/java/com/gatherly/api/GatherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatherly.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
