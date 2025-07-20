6289830d6026577d4d060c5fee6ea5057f567cd7
SamriekLeeuwin <semriekk@gmail.com>
2025-07-20 22:08:16 +0200

    Implement User entity with JPA annotations, including fields for id, role, email, password, and isActive status.

--- gatherly-backend/gatherly-api/src/main/java/com/gatherly/api/model/user.java
+++ gatherly-backend/gatherly-api/src/main/java/com/gatherly/api/model/user.java
@@ -1,12 +1,41 @@
-
+// Jpa entity for user
 package com.gatherly.api;
 
+import jakarta.persistence.Entity;
 
-import 
 
 @Entity
-public class user{
+public class User{
 
-private Long username unique=true  max= 25 
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@Enumerated(EnumType.STRING)
+private Role role;
+
+@Column(unique = true, nullable = false, length = 25,  max = 25)
+private String email;
+
+
+@Column(nullable = false, length = 50)
+private String password;
+
+@Column(nullable = false)
+private Boolean isActive;
+
+
+
+public User(String email, String password, Role role){
+    this.email = email;
+    this.password = password;
+    this.role = role;
+    this.isActive = true;
+}
+
+
+
+
+
 
 }
